618de72b27f37890686035cbd10410e707cbb647
MatV <matvercau@gmail.com>
2023-02-21 15:55:42 +0100

    mapping class Indenteur et Tapoteur

--- src/main/java/model/Tapoteur.java
+++ src/main/java/model/Tapoteur.java
@@ -1,14 +1,18 @@
 package model;
 
+import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.Table;
 
 @Entity
 @Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="rang_tapoteur", columnDefinition = "ENUM('fidele','indenteur','compileur','grandDev')")
+@Table(name="Rang")
 public class Tapoteur {
 	
 	@Id
