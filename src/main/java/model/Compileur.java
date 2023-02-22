6239233bb62d4e6d2bd4dc4bcf4f941c3f9fb6f9
MatV <matvercau@gmail.com>
2023-02-22 12:16:29 +0100

    mapping Compileur + GrandDev

--- src/main/java/model/Compileur.java
+++ src/main/java/model/Compileur.java
@@ -1,6 +1,13 @@
 package model;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("R3")
 public class Compileur extends Tapoteur {
+	
+	public Compileur() {}
 
 	public Compileur(String nom, String prenom, String adresse, String dateAdhesion, String login, String password) {
 		super(nom, prenom, adresse, dateAdhesion, login, password);
--- src/main/java/model/GrandDev.java
+++ src/main/java/model/GrandDev.java
@@ -1,9 +1,18 @@
 package model;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("R4")
 public class GrandDev extends Tapoteur {
 
 	private String colPull;
 	
+	
+	
+	public GrandDev() {}
+
 	public GrandDev(String nom, String prenom, String adresse, 
 					String dateAdhesion, String login, String password, String colPull) {
 		super(nom, prenom, adresse, dateAdhesion, login, password);
