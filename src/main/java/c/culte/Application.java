8c733f5440b63e0da9586c0678112f067435c87b
ThomasF <feroldithomas@gmail.com>
2023-03-14 09:56:34 +0100

    Auto stash before merge of "main" and "origin/main"

--- src/main/java/c/culte/Application.java
+++ src/main/java/c/culte/Application.java
@@ -54,65 +54,15 @@ public class Application {
 	}
 
 	static Tapoteur connected;
-
-
-
 	public static void main(String[] args) {
-
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-
-		IDAOCulte IDAOc = context.getBean(IDAOCulte.class);
-		IDAODemande IDAOd = context.getBean(IDAODemande.class);
-		IDAOEvenement IDAOe = context.getBean(IDAOEvenement.class);
-		IDAOTapoteur IDAOt = context.getBean(IDAOTapoteur.class);
-
-
-
-		//Create objects
-		Adresse a1 = new Adresse ("14","Rue de Christian Clavier","Limoges","87000","Fronce");
-		Adresse a2 = new Adresse ("13","Rue du Mulot","Limoges","87000","Fronce");
-		Adresse a3 = new Adresse ("55","Rue du Port","Limoges","87000","Fronce");
-
-
-		GrandDev Dave = new GrandDev ("Davidson","Dave",a1,LocalDate.now(),"dev","dev","saumon");
-		Indenteur JordanRolland = new Indenteur ("Rolland","Jordan",a2,LocalDate.now(),"rolland","rolland");
-		Compileur MarianneGuisset = new Compileur ("Guisset","Marianne",a1,LocalDate.now(),"guisset","guisset");
-		Fidele FrancisCatrel = new Fidele("Francis", "Catrel", a3, LocalDate.now(), "catrel", "catrel4ever");
-
-		Demande demande = new Demande(135, "achat d'un couvre chef svp");
-		JordanRolland.setDemande(demande);
-		demande.setIndenteur(JordanRolland);
-
-		Evenement evenement = new Evenement("fete", LocalDate.now(), "Toulouse", Activite.rassemblement);
-
-		Culte culteJava = new Culte(284.5, 4);
-		culteJava.getListEvenement().add(evenement);
-		Collections.addAll(culteJava.getListTapoteur(), JordanRolland,MarianneGuisset,FrancisCatrel);
-
-
-
-		culteJava = IDAOc.save(culteJava);
-
-		Dave.setCulte(culteJava);
-		Dave = (GrandDev) IDAOt.save(Dave);
-
-		culteJava.setDave(Dave);
-		IDAOc.save(culteJava);
-
-
-		JordanRolland.setCulte(culteJava);
-		IDAOt.save(JordanRolland);
-
-		MarianneGuisset.setCulte(culteJava);
-		IDAOt.save(MarianneGuisset);
-
-		FrancisCatrel.setCulte(culteJava);
-		IDAOt.save(FrancisCatrel);
-
-		evenement.setCulte(culteJava);
-		IDAOe.save(evenement);
-
-
+	AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+	
+	IDAOCulte IDAOc = context.getBean(IDAOCulte.class);
+	IDAODemande IDAOd = context.getBean(IDAODemande.class);
+	IDAOEvenement IDAOe = context.getBean(IDAOEvenement.class);
+	IDAOTapoteur IDAOt = context.getBean(IDAOTapoteur.class);
+	
+	
 
 		System.out.println("test");
 
