1fd172e6320b0dff2f76f6bd2ac9ba5718489389
utopiathe <anthony.rouyer33.16@gmail.com>
2023-03-15 15:09:26 +0100

    modif demande response

--- src/main/java/c/culte/response/DemandeResponse.java
+++ src/main/java/c/culte/response/DemandeResponse.java
@@ -4,7 +4,44 @@ import c.culte.model.Statut;
 
 public class DemandeResponse {
 	
+	private Integer id;
+	private String nom;
+	private double montant;
+	private String descriptif;
 	private Statut statut;
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public double getMontant() {
+		return montant;
+	}
+
+	public void setMontant(double montant) {
+		this.montant = montant;
+	}
+
+	public String getDescriptif() {
+		return descriptif;
+	}
+
+	public void setDescriptif(String descriptif) {
+		this.descriptif = descriptif;
+	}
 
 	public Statut getStatut() {
 		return statut;
