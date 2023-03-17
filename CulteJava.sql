cbea2a1fab6f05efa74870573822f06ecde03fe2
Marianne Guisset <marianneguisset@aol.com>
2023-03-17 16:51:52 +0100

    bdd culte

---
+++ CulteJava.sql
@@ -0,0 +1,6 @@
+INSERT INTO `tapoteur` (`rang_tapoteur`, `numero`, `voie`, `cp`, `ville`, `pays`, `date_adhesion`, `login`, `password`, `prenom`, `nom`) VALUES
+('grandDev', '12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'dev', 'dev1', 'Dave', 'Davinson'),
+('compileur', '12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'mathias', 'v', 'Mathias', 'V'),
+('compileur', '12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'thomas', 'f', 'Thomas', 'F'),
+('compileur', '12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'anthony', 'r', 'Anthony', 'R'),
+('compileur', '12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'crockdur', 'guigui', 'Marianne', 'Guisset');
