379e2d61c7fcf853f466915d3324d06f62193fb7
Marianne Guisset <marianneguisset@aol.com>
2023-04-01 12:38:49 +0200

    CrossOrigin manquant dans certains fichiers (merci Lina)

--- CultejavaBack/src/main/java/c/culte/api/DemandeApiController.java
+++ CultejavaBack/src/main/java/c/culte/api/DemandeApiController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/demande")
+@CrossOrigin("*")
 public class DemandeApiController {
 	
 	@Autowired
